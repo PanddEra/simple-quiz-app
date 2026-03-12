1b34c7262f7088a746fc72f00bdcb4d9ab946702
PanddEra <antonovmykola2008@gmail.com>
2026-03-12 11:19:57 +0100

    updated

--- application/scripts/js/modules/questionService.js
+++ application/scripts/js/modules/questionService.js
@@ -5,6 +5,20 @@ const questionService = {
 },
 
     _generateQuestionTemplate: (question) => {
+        return
+        `<div class="card">
+            <div class="card-header">
+                    ${question.text}
+            </div>
+            <div class="card-body">
+                <div class="form-check">
+                    ${question.options.forEach(q => {
+                        <input className="form-check-input" type="radio" name="radioDefault" id="radioDefault1"/>
+                        <label className="form-check-label" htmlFor="radioDefault1">${q.text}</label>
+                    })}
+                </div>
+            </div>
+        </div>`
+    }
+}
 
-}
-}
--- application/scripts/js/modules/requestHandler.js
+++ application/scripts/js/modules/requestHandler.js
@@ -1,4 +1,7 @@
 const requestHandler = {
-
+    async fetchGetQuestions(url) {
+       const response = await fetch(url);
+       //TODO request handler
+    }
 }
 export default requestHandler;
